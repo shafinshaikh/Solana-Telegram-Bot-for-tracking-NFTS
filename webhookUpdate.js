3ee55cfef8daa19a6c99d9ed6cd0750b32407774
SHAFIN SHAIKH <shafinshaikh25@gmail.com>
2024-06-05 15:56:57 +0530

    update webhook file for env

--- webhookUpdate.js
+++ webhookUpdate.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const HELIUS_API = process.env.HELIUS_API;
+
 
 const editWebhook = async () => {
     try {
@@ -6,7 +8,7 @@ const editWebhook = async () => {
       const addresses = JSON.parse(fs.readFileSync('addresses.json', 'utf8'));
   
       const response = await fetch(
-        "https://api.helius.xyz/v0/webhooks/?api-key=1e5c6ee1-43bd-46c1-9914-1ef8ff9e9c8e",
+        `https://api.helius.xyz/v0/webhooks/?api-key=${HELIUS_API}`,
         {
           method: 'PUT',
           headers: {
